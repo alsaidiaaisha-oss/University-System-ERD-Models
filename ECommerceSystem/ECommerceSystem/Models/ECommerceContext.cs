f566dfcbb546a0325be625fb4f723508a12fe7c7
Aaisha Khalfan Saif AlSaidi <alsaidiaaisha@gmail.com>
2026-07-06 16:35:12 +0400

    Added Entity Framework and database

---
+++ ECommerceSystem/ECommerceSystem/Models/ECommerceContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ECommerceSystem.Models;
+
+namespace ECommerceSystem.Models
+{
+    public class ECommerceContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost\\SQLEXPRESS;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+    }
+}
+
